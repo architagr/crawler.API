fdc650ce7f16ac50481ecfa85c5fc410d809dc80
Gaurav Agarwal <grwlgrv123@gmail.com>
2023-04-04 20:22:28 +0530

    redis error

--- main.go
+++ main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+
+	//cacheservice "jobcrawlerApi/services"
 
 	"github.com/gorilla/mux"
 	"github.com/twilio/twilio-go"
@@ -20,29 +25,53 @@ type Message struct {
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/", func(w http.ResponseWriter, p *http.Request) {
 		w.Write([]byte("Hello, world!"))
 	})
 
+	// r.HandleFunc("/set", func(w http.ResponseWriter, p *http.Request) {
+	// 	query := p.URL.Query()
+	// 	data, present := query["id"]
+	// 	if !present || len(data) == 0 {
+	// 		fmt.Println("data not present")
+	// 	}
+	// 	cacheservice.Set(data[0])
+	// })
+
+	// r.HandleFunc("/get", func(w http.ResponseWriter, p *http.Request) {
+	// 	var res = cacheservice.Get("1")
+	// 	fmt.Fprint(w, res)
+	// })
+
 	r.HandleFunc("/sms", func(w http.ResponseWriter, p *http.Request) {
 		fmt.Println("SMS API initiated")
+
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		//get request param
+		body, err := ioutil.ReadAll(p.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusBadRequest)
+			return
+		}
+		defer p.Body.Close()
+		phoneNo := string(body)
+		fmt.Println("mobileno: " + phoneNo)
 
 		accountSaid := "AC67b72d1e3041953fc3fb10da36a0c5a0"
-		authToken := "f5f3e764930b0224d35f1b8439ba9a7a"
+		authToken := "92a71371a777646a65d102c469f70bb4"
 		client := twilio.NewRestClientWithParams(twilio.ClientParams{
 			Username: accountSaid,
 			Password: authToken,
 		})
-
+		randno := rand.Intn(9999)
 		params := &openapi.CreateMessageParams{}
-		params.SetTo("+919950458542")
+		params.SetTo(phoneNo)
 		params.SetFrom("+14344045914")
-		params.SetBody("Message")
+		params.SetBody("OTP received to login JobPortal is " + strconv.Itoa(randno))
 
-		_, err := client.Api.CreateMessage(params)
-		if err != nil {
+		_, err1 := client.Api.CreateMessage(params)
+		if err1 != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			fmt.Println(err.Error())
 			return
@@ -51,7 +80,7 @@ func main() {
 		}
 
 		json.NewEncoder(w).Encode("Message sent successfully")
-	})
+	}).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
