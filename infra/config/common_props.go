f10d86e4639ab3fa1f3a2ae377fdbb3bba8c7b5e
Archit Agarwal <architagr@gmail.com>
2023-06-08 23:30:46 +0530

    add basic infra code

---
+++ infra/config/common_props.go
@@ -0,0 +1,165 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	awscdk "github.com/aws/aws-cdk-go/awscdk/v2"
+	apigateway "github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
+	acm "github.com/aws/aws-cdk-go/awscdk/v2/awscertificatemanager"
+	route53 "github.com/aws/aws-cdk-go/awscdk/v2/awsroute53"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type DatabaseModel struct {
+	connectionString, dbname, collectionName string
+}
+
+func (db *DatabaseModel) GetConnectionString() string {
+	return db.connectionString
+}
+
+func (db *DatabaseModel) GetDbName() string {
+	return db.dbname
+}
+func (db *DatabaseModel) GetCollectionName() string {
+	return db.collectionName
+}
+
+type CommonStackProps struct {
+	IsLocal string
+	Stage   *apigateway.StageOptions
+}
+type DomainDetails struct {
+	RecordName, Url string
+}
+
+type Domain struct {
+	BaseApi                                     string
+	JobApiDomain, UserApiDomain, LoginApiDomain DomainDetails
+}
+type InfraEnv struct {
+	StackNamePrefix string
+	Domains         Domain
+	CommonStackProps
+	HostedZoneId   string
+	CertificateArn string
+}
+
+type CommonProps struct {
+	JobAPIDB, LoginAPIDB, UserAPIDB DatabaseModel
+	awscdk.StackProps
+	InfraEnv
+}
+
+const baseDomain = "inventory-management.click"
+
+func GetCommonProps(app awscdk.App) *CommonProps {
+	return &CommonProps{
+		JobAPIDB: DatabaseModel{
+			connectionString: "mongodb+srv://webscrapper:WebScrapper123@cluster0.xzvihm7.mongodb.net/?retryWrites=true&w=majority",
+			dbname:           "webscrapper",
+			collectionName:   "",
+		},
+		LoginAPIDB: DatabaseModel{
+			connectionString: "mongodb+srv://webscrapper:WebScrapper123@cluster0.xzvihm7.mongodb.net/?retryWrites=true&w=majority",
+			dbname:           "webscrapper",
+			collectionName:   "",
+		},
+		UserAPIDB: DatabaseModel{
+			connectionString: "mongodb+srv://webscrapper:WebScrapper123@cluster0.xzvihm7.mongodb.net/?retryWrites=true&w=majority",
+			dbname:           "webscrapper",
+			collectionName:   "",
+		},
+		StackProps: env(app),
+		InfraEnv: InfraEnv{
+			HostedZoneId:    "Z00125422MSTTNZDZ5PRV",
+			CertificateArn:  "arn:aws:acm:ap-south-1:675174225340:certificate/4995bc1a-da1b-42b1-b8d8-504ad0b9826b",
+			StackNamePrefix: "crawler-api",
+			Domains: Domain{
+				BaseApi: baseDomain,
+				JobApiDomain: DomainDetails{
+					RecordName: "job-api",
+					Url:        fmt.Sprintf("job-api.%s", baseDomain),
+				},
+				LoginApiDomain: DomainDetails{
+					RecordName: "login-api",
+					Url:        fmt.Sprintf("login-api.%s", baseDomain),
+				},
+				UserApiDomain: DomainDetails{
+					RecordName: "user-api",
+					Url:        fmt.Sprintf("user-api.%s", baseDomain),
+				},
+			},
+			CommonStackProps: CommonStackProps{
+				IsLocal: os.Getenv("isLocal"),
+				Stage: &apigateway.StageOptions{
+					StageName: jsii.String("Dev"),
+				},
+			},
+		},
+	}
+}
+
+func AllowCors() *apigateway.ResourceOptions {
+	return &apigateway.ResourceOptions{
+		DefaultCorsPreflightOptions: GetCorsPreflightOptions(),
+		DefaultMethodOptions:        &apigateway.MethodOptions{},
+	}
+}
+
+func GetCorsPreflightOptions() *apigateway.CorsOptions {
+	return &apigateway.CorsOptions{
+		AllowOrigins:     apigateway.Cors_ALL_ORIGINS(),
+		AllowMethods:     apigateway.Cors_ALL_METHODS(),
+		AllowHeaders:     jsii.Strings("Content-Type", "Authorization", "X-Amz-Date", "X-Api-Key"),
+		AllowCredentials: jsii.Bool(true),
+	}
+}
+
+func CreateAcmCertificate(scope constructs.Construct, props *InfraEnv) acm.ICertificate {
+	return acm.Certificate_FromCertificateArn(scope, jsii.String("clientApiCertificate"), &props.CertificateArn)
+}
+
+func GetHostedZone(scope constructs.Construct, id *string, props InfraEnv) route53.IHostedZone {
+	return route53.PublicHostedZone_FromHostedZoneAttributes(scope, id, &route53.HostedZoneAttributes{
+		HostedZoneId: jsii.String(props.HostedZoneId),
+		ZoneName:     jsii.String(props.Domains.BaseApi),
+	})
+}
+
+// env determines the AWS environment (account+region) in which our stack is to
+// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+func env(app awscdk.App) awscdk.StackProps {
+	accountId := fmt.Sprint(app.Node().TryGetContext(jsii.String("ACCOUNT_ID")))
+	region := fmt.Sprint(app.Node().TryGetContext(jsii.String("REGION")))
+	project := fmt.Sprint(app.Node().TryGetContext(jsii.String("PROJECT")))
+	// If unspecified, this stack will be "environment-agnostic".
+	// Account/Region-dependent features and context lookups will not work, but a
+	// single synthesized template can be deployed anywhere.
+	//---------------------------------------------------------------------------
+	// return nil
+
+	// Uncomment if you know exactly what account and region you want to deploy
+	// the stack to. This is the recommendation for production stacks.
+	//---------------------------------------------------------------------------
+	return awscdk.StackProps{
+		Env: &awscdk.Environment{
+			Account: jsii.String(accountId),
+			Region:  jsii.String(region),
+		},
+		Tags: &map[string]*string{
+			"project": jsii.String(project),
+		},
+	}
+
+	// Uncomment to specialize this stack for the AWS Account and Region that are
+	// implied by the current CLI configuration. This is recommended for dev
+	// stacks.
+	//---------------------------------------------------------------------------
+	// return &awscdk.Environment{
+	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
+	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
+	// }
+}
