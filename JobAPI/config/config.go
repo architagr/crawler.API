3cf4ce90a5be05f1b8fdd63f6edb0f036f808295
Archit Agarwal <architagr@gmail.com>
2023-12-17 21:11:53 +0530

    modify the use of lambda_task_root use

--- JobAPI/config/config.go
+++ JobAPI/config/config.go
@@ -30,7 +30,7 @@ const (
 )
 
 func InitConfig() {
-	_, ok := os.LookupEnv("LAMBDA_TASK_ROOT")
+	_, ok := os.LookupEnv(isLambdaEnvKey)
 	env = &Config{
 		databaseConnectionString: os.Getenv(databaseConnectionStringKey),
 		databaseName:             os.Getenv(databaseNameKey),
